7d00b4437dc88c9871b30892edf50c3c0eeca84a
Murat Berk <132847833+Mrtytt@users.noreply.github.com>
2024-11-21 10:26:23 +0300

    Update

---
+++ 5_Donem_Ders_Notlari/VTYS/VTYS_8.Hafta.sql
@@ -0,0 +1,29 @@
+-- select distinct sinif from ogrenci
+
+-- select * from ogrenci where cinsiyet='K'
+
+-- select ograd,ogrsoyad, sinif from ogrenci where sinif='10A' or sinif='10B'
+
+-- select ograd,ogrsoyad,ogrno as 'Okul Numarasý' from ogrenci
+
+-- select ograd+ogrsoyad as 'Ad Soyad' from ogrenci
+
+-- select * from ogrenci where ograd like 'A%'
+
+-- select * from kitap where sayfasayisi between 50 and 200 
+
+-- select * from ogrenci where ograd in ('Fidan','Ýsmail')
+
+-- select * from ogrenci where ograd like  '[ADK]%'
+
+-- select * from ogrenci where sinif = '10A' order by ogrno Desc
+
+-- select TOP 10 * from ogrenci 
+
+-- select top 10 ograd,ogrsoyad,dtarih from ogrenci 
+
+-- select top 1 * from kitap order by sayfasayisi desc
+
+-- select top 1 ograd,ogrsoyad,dtarih from ogrenci order by dtarih desc
+
+-- devamý yapýlacak
